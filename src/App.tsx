b25bf10ff83419923fecc0671fd25fe2ab2f573a
Egor Ogarkov <egor.ogarkov@jetbrains.com>
2025-09-17 15:44:04 +0200

    node configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import Header from './components/Header'
 import NavigationManager from './components/NavigationManager'
-import { FormProvider } from './context/FormContext'
 
 function App() {
   return (
-    <FormProvider>
-      <div className="h-screen bg-neutral-900/60 backdrop-blur">
-        <Header />
-        <NavigationManager />
-      </div>
-    </FormProvider>
+    <div className="h-screen bg-neutral-900/60 backdrop-blur">
+      <Header />
+      <NavigationManager />
+    </div>
   )
 }
 
